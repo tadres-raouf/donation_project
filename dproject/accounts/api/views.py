7cb6fd47d43bf38ec02dd61f795f07d7aa0915e0
shady <shadyshawky90@gmail.com>
2025-06-03 03:43:23 +0300

    created accounts views

---
+++ dproject/accounts/api/views.py
@@ -0,0 +1,32 @@
+from rest_framework import generics
+from .serializers import UserRegistrationSerializer, CustomTokenObtainPairSerializer, UserProfileSerializer
+from accounts.models import UserProfile
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        profile = request.user.profile
+        serializer = UserProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request):
+        profile = request.user.profile
+        serializer = UserProfileSerializer(profile, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
