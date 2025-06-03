f78ae935ca92febbc14789ef444de910404769c6
shady <shadyshawky90@gmail.com>
2025-06-03 03:47:04 +0300

    created accounts done

--- dproject/dproject/urls.py
+++ dproject/dproject/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.api.urls')),
     
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
